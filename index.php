3749eff35228b21adcb206981e6c374395046e97
Jorrgejimenezm <156658400+Jorrgejimenezm@users.noreply.github.com>
2025-05-07 16:58:05 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <html>
 <head>
 <title>Tajamar Media</title>
+<style>
+    h1{
+        color:yellow;
+    }
+</style>
 </head>
 <body>
 <h1 align="center">Buenar tardes, estudiantes del Master de Tajamar </h1>
