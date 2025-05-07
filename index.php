76800531c63bc768fe876ce7e73a8f7d772e16e3
Jorrgejimenezm <156658400+Jorrgejimenezm@users.noreply.github.com>
2025-05-07 16:41:56 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <title>Tajamar Media</title>
 </head>
 <body>
-<h1 align="center">Buenos días, estudiantes </h1>
+<h1 align="center">Buenar tardes, estudiantes del Master de Tajamar </h1>
 <br>
 <div align="center">
 <img src="playa.jpg">
